4b5cf137b5f31357ce66aa20515581975ab2008a
kharylindias <kharylin.dias@ibm.com>
2026-01-14 15:05:10 -0300

    feat: integração com a IP

--- force-app/main/default/lwc/loanSimulator/loanSimulator.js
+++ force-app/main/default/lwc/loanSimulator/loanSimulator.js
@@ -1,10 +1,11 @@
 import { LightningElement, track } from 'lwc';
+import simular from '@salesforce/apex/LoanController.simular';
 
 export default class LoanSimulator extends LightningElement {
 
     @track sliderValorSolicitado = 1000;
     @track sliderQtdParcelas = 12;
-    resultado;
+    @track resultadoParcela;
 
     handleSliderValorChange(event) {
         this.sliderValorSolicitado = event.target.value;
@@ -14,8 +15,30 @@ export default class LoanSimulator extends LightningElement {
         this.sliderQtdParcelas = event.target.value;
     }
 
-    calcular(){
-        console.log("Valor:", this.sliderValorSolicitado);
+    async calcularSimulacao(){
+        this.resultadoParcela = null;
+      
+        const inputDoIP = {
+            valor: this.sliderValorSolicitado,
+            meses: this.sliderQtdParcelas
+        };
+        
+        try {
+
+            const result = await simular({ dadosDaTela: inputDoIP });
+        
+            console.log('JSON:', JSON.stringify(result));
+            
+            if (result?.valorParcela) {
+                this.resultadoParcela = result.valorParcela; // mostra valor na tela
+            } else {
+                console.error('O retorno veio vazio ou sem a chave esperada:', result);
+            }
+        }
+        catch (error) {
+            console.error('Erro na chamada Apex:', error);
+        }
+
     }
 
 }
